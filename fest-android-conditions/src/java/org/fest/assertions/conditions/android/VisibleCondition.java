1a60322dfe6a6d540eedd3b6e6f5247926ce5cde
DD <timnew.wti@gmail.com>
2013-12-10 20:51:09 +0800

    Reformat

--- fest-android-conditions/src/java/org/fest/assertions/conditions/android/VisibleCondition.java
+++ fest-android-conditions/src/java/org/fest/assertions/conditions/android/VisibleCondition.java
@@ -9,11 +9,12 @@ import static java.lang.String.format;
 
 public class VisibleCondition extends Condition<View> {
     public static final String DESCRIPTION_TEMPLATE = "visible \"%s\"";
+
     public static final VisibleCondition INVISIBLE = new VisibleCondition(false);
     public static final VisibleCondition VISIBLE = new VisibleCondition(true);
-
     @SuppressWarnings("unchecked")
     public static final HasPropertyConditionPrimitive NOT_DISPLAYED = new HasPropertyConditionPrimitive("visibility", GONE);
+
     private final boolean expectedVisible;
 
     public VisibleCondition(boolean expectedVisible) {
